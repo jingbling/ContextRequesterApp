3340b546f3ade561c93a7be04a302681a5b96f32
Jing Shen <jingshen.meche@gmail.com>
2013-02-03 23:14:24 -0800

    Update to use IntentService and JSON format for application request

--- src/com/example/ContextRequesterApp/ContextRequestAct.java
+++ src/com/example/ContextRequesterApp/ContextRequestAct.java
@@ -1,22 +1,17 @@
 package com.example.ContextRequesterApp;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.*;
 import android.os.Process;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.Toast;
-import org.jingbling.ContextEngine.IContextService;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ContextRequestAct extends Activity implements View.OnClickListener {
     // declare variables
@@ -28,60 +23,6 @@ public class ContextRequestAct extends Activity implements View.OnClickListener 
 
     private Spinner bClassifierSelection;
     private Spinner bContextSelection;
-
-    private CheckBox featureSelectBox1;
-    private CheckBox featureSelectBox2;
-    private CheckBox featureSelectBox3;
-    private CheckBox featureSelectBox4;
-    private int numCheckBoxes = 4;
-
-    private IContextService targetContextService;
-    private Bundle appThreadArgs;
-    private Message appThreadMsg;
-    private ArrayList<String> stringArrayInput = new ArrayList<String>();
-
-    // Temp variables for debugging
-    String ACT_TAG="ContextRequestAct";
-
-    private ServiceConnection appServiceConn = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.v(ACT_TAG, "SERVICE CONNECTED");
-            targetContextService = IContextService.Stub.asInterface(service);
-            Toast.makeText(getApplicationContext(), "service connected",
-                    Toast.LENGTH_LONG);
-
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            Log.v(ACT_TAG, "SERVICE DISCONNECTED");
-            targetContextService = null;
-            Toast.makeText(getApplicationContext(), "service disconnected",
-                    Toast.LENGTH_LONG);
-        }
-    };
-
-    private volatile Looper appServiceLooper;
-    private volatile ServiceHandler appServiceHandler;
-
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
-            super(looper);
-        }
-
-        public void handleMessage(Message msg) {
-            Bundle args = (Bundle)msg.obj;
-
-            if ( args.containsKey("context_case") ) {
-                remoteContextCall(args.getStringArrayList("FeaturesList"), args.getString("ClassifierName"), args.getString("ContextGroup"));
-            }
-            else if ( args.containsKey("train_data") ) {
-                remoteActivityCall(args.getStringArrayList("FeaturesList"), args.getString("ContextGroup"));
-            }
-        }
-    }
 
     /**
      * Called when the activity is first created.
@@ -101,11 +42,11 @@ public class ContextRequestAct extends Activity implements View.OnClickListener 
 
         bLaunchContextActBtn = (Button)findViewById(R.id.btnGetContext);
         bLaunchContextActBtn.setOnClickListener(this);
-        bLaunchContextActBtn.setEnabled(false);
+//        bLaunchContextActBtn.setEnabled(false);
 
         bLaunchTrainingActBtn = (Button)findViewById(R.id.btnTrainData);
         bLaunchTrainingActBtn.setOnClickListener(this);
-        bLaunchTrainingActBtn.setEnabled(false);
+//        bLaunchTrainingActBtn.setEnabled(false);
 
         bCloseAppBtn = (Button)findViewById(R.id.btnCloseApp);
         bCloseAppBtn.setOnClickListener(this);
@@ -113,93 +54,74 @@ public class ContextRequestAct extends Activity implements View.OnClickListener 
         bClassifierSelection = (Spinner) findViewById(R.id.classifier_spinner);
         bContextSelection = (Spinner) findViewById(R.id.context_spinner);
 
-        featureSelectBox1 = (CheckBox)findViewById(R.id.feature_checkbox1);
-        featureSelectBox2 = (CheckBox)findViewById(R.id.feature_checkbox2);
-        featureSelectBox3 = (CheckBox)findViewById(R.id.feature_checkbox3);
-        featureSelectBox4 = (CheckBox)findViewById(R.id.feature_checkbox4);
-
-        HandlerThread hthr =
-                new HandlerThread("StartedContextLooperService2Thread",
-                        android.os.Process.THREAD_PRIORITY_BACKGROUND);
-        hthr.start();
-        appServiceLooper = hthr.getLooper();
-        appServiceHandler = new ServiceHandler(appServiceLooper);
-        appThreadArgs = new Bundle();
     }
 
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message message) {
+            Bundle output = message.getData();
+            if (output != null) {
+                Toast.makeText(getApplicationContext(),
+                        "Classified: " + output.getString("label"), Toast.LENGTH_LONG)
+                        .show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Classification failed",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        };
+    };
 
     @Override
     public void onClick(View v) {
         switch ( v.getId() ) {
             case R.id.btnBindService:
-                bindService(new Intent(IContextService.class.getName()),
-                        appServiceConn, Context.BIND_AUTO_CREATE);
-                bBindServiceBtn.setEnabled(false);
-                bLaunchContextActBtn.setEnabled(true);
-                bLaunchTrainingActBtn.setEnabled(true);
-                bUnbindServiceBtn.setEnabled(true);
+//                bindService(new Intent(IContextService.class.getName()),
+//                        appServiceConn, Context.BIND_AUTO_CREATE);
+//                bBindServiceBtn.setEnabled(false);
+//                bLaunchContextActBtn.setEnabled(true);
+//                bLaunchTrainingActBtn.setEnabled(true);
+//                bUnbindServiceBtn.setEnabled(true);
                 break;
             case R.id.btnUnbindService:
-                unbindService(appServiceConn);
-                bBindServiceBtn.setEnabled(true);
-                bLaunchContextActBtn.setEnabled(false);
-                bLaunchTrainingActBtn.setEnabled(false);
-                bUnbindServiceBtn.setEnabled(false);
+//                unbindService(appServiceConn);
+//                bBindServiceBtn.setEnabled(true);
+//                bLaunchContextActBtn.setEnabled(false);
+//                bLaunchTrainingActBtn.setEnabled(false);
+//                bUnbindServiceBtn.setEnabled(false);
                 break;
             case R.id.btnGetContext:
+                // Pass intent to service
+                Bundle bundleInput = new Bundle();
+                Intent InputIntent = new Intent("org.jingbling.ContextEngine.ContextService");
 
-                // Use selected variables to pass arguments to remote call
-                // Features List
+                // Create JSON format as input
+                JSONObject jsonInput = new JSONObject();
+                try {
+                    jsonInput.put("classifier", bClassifierSelection.getSelectedItem().toString());
+                    jsonInput.put("contextGroup", bContextSelection.getSelectedItem().toString());
+                    JSONArray featuresArray = new JSONArray();
+                    // ??? TBR - for now hardcode features used, to be replaced with selectable list
+                    featuresArray.put("AvgFFTx");
+                    featuresArray.put("AvgFFTy");
+                    featuresArray.put("AvgFFTz");
+                    jsonInput.put("features", featuresArray);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.v("GETContextButton", jsonInput.toString());
+                bundleInput.putString("JSONInput",jsonInput.toString());
 
-                appThreadMsg = appServiceHandler.obtainMessage();
-                appThreadArgs.clear();
-                appThreadArgs.putBoolean("context_case", true);
+                // Create a new Messenger for the communication back
+                Messenger messenger = new Messenger(handler);
+                InputIntent.putExtra("MESSENGER", messenger);
 
-                // For now, hard-code arguments to test remote call
-                stringArrayInput.clear();
-                // Add checked features to array list
-                if (featureSelectBox1.isChecked())
-                    stringArrayInput.add(featureSelectBox1.getText().toString());
-                if (featureSelectBox2.isChecked())
-                    stringArrayInput.add(featureSelectBox2.getText().toString());
-                if (featureSelectBox3.isChecked())
-                    stringArrayInput.add(featureSelectBox3.getText().toString());
-                if (featureSelectBox4.isChecked())
-                    stringArrayInput.add(featureSelectBox4.getText().toString());
+                InputIntent.putExtras(bundleInput);
+                startService(InputIntent);
 
-                appThreadArgs.putStringArrayList("FeaturesList", stringArrayInput);
-                appThreadArgs.putString("ClassifierName",bClassifierSelection.getSelectedItem().toString());
-                appThreadArgs.putString("ContextGroup",bContextSelection.getSelectedItem().toString());
-
-                appThreadMsg.obj = appThreadArgs;
-                appServiceHandler.sendMessage(appThreadMsg);
                 break;
             case R.id.btnTrainData:
-//
-                // Use selected variables to pass arguments to remote call
-                // Features List
 
-                appThreadMsg = appServiceHandler.obtainMessage();
-                appThreadArgs.clear();
-                appThreadArgs.putBoolean("train_data", true);
-
-                // For now, hard-code arguments to test remote call
-                stringArrayInput.clear();
-                // Add checked features to array list
-                if (featureSelectBox1.isChecked())
-                    stringArrayInput.add(featureSelectBox1.getText().toString());
-                if (featureSelectBox2.isChecked())
-                    stringArrayInput.add(featureSelectBox2.getText().toString());
-                if (featureSelectBox3.isChecked())
-                    stringArrayInput.add(featureSelectBox3.getText().toString());
-                if (featureSelectBox4.isChecked())
-                    stringArrayInput.add(featureSelectBox4.getText().toString());
-
-                appThreadArgs.putStringArrayList("FeaturesList", stringArrayInput);
-                appThreadArgs.putString("ContextGroup","activity");
-
-                appThreadMsg.obj = appThreadArgs;
-                appServiceHandler.sendMessage(appThreadMsg);
                 break;
             case R.id.btnCloseApp:
                 Process.killProcess(Process.myPid());
@@ -213,33 +135,4 @@ public class ContextRequestAct extends Activity implements View.OnClickListener 
         super.onDestroy();
     }
 
-    // Calls to remote service
-
-    private void remoteContextCall(List<String> featuresToUse, String classifierToUse, String contextGroup) {
-        try {
-            String contextOut = targetContextService.getContext(featuresToUse, classifierToUse, contextGroup);
-            Toast.makeText(getApplicationContext(),
-                    "Context = "+contextOut,
-                    Toast.LENGTH_LONG).show();
-        }
-        catch ( RemoteException ex ) {
-            Toast.makeText(getApplicationContext(),
-                    ex.toString(),
-                    Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void remoteActivityCall(List<String> featuresToUse, String contextGroup) {
-        try {
-            targetContextService.gatherTrainingData(featuresToUse, contextGroup, "trainingDataFile.txt");
-            Toast.makeText(getApplicationContext(),
-                    "Launch",
-                    Toast.LENGTH_LONG).show();
-        }
-        catch ( RemoteException ex ) {
-            Toast.makeText(getApplicationContext(),
-                    ex.toString(),
-                    Toast.LENGTH_LONG).show();
-        }
-    }
 }
--- src/org/jingbling/ContextEngine/IContextService.aidl
+++
@@ -1,7 +0,0 @@
-package org.jingbling.ContextEngine;
-
-interface IContextService
-{
-    String getContext(in List<String> featuresToUse, String classifierToUse, String contextGroup);
-    void gatherTrainingData(in List<String> featuresToUse, String contextGroup, String filename);
-}
